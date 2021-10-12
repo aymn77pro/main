7ffab0a41d2b9ee2730dc1f415a967f2bcea74ae
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:00:48 +0300

    get dozen numbier

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+
+fun main(){
+
+ var apple= readLine()!!
+    var dozens= apple.toInt()/12//num of dozens
+    println("numer of the dozens = "+ dozens)
+    var applesRemaing =apple.toInt()%12// num of apples remaing
+    println("numer of the remaing apple = "+ applesRemaing)
+}
--- src/main1.kt
+++
@@ -1,3 +0,0 @@
-fun main1(){
-    println("hi")
-}
